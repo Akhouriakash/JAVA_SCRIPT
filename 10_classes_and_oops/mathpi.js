4c8689255e5c57e324d4983c86ccd5a0071552bf
akash <akhouriakash10@gmail.com>
2025-08-18 17:20:43 +0530

    thirt commit

---
+++ 10_classes_and_oops/mathpi.js
@@ -0,0 +1,32 @@
+const descripter = Object.getOwnPropertyDescriptor(Math , "PI") // tells the hiidden properties of objects
+
+// console.log(Math.PI)
+// Math.PI = 5
+// console.log(Math.PI)
+
+// console.log(descripter)
+
+const chai = {  // object
+    name : 'ginger',  // prop
+    price: 250,
+    isAvailable : true ,
+
+    orderChai: function(){
+        console.log("chai nhi bni")
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai ,"name"))
+Object.defineProperty(chai,'name' ,{
+    // writable: false,
+    enumerable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai ,"name"))
+
+for (let [key,value] of Object.entries(chai)) {
+
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
